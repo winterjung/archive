659c5b357ee6725fd0807b4e5a8d59c6f2658acf
winterJ <wintermy201@naver.com>
2017-03-17 00:49:02 +0900

    Delete comment

--- 02 인터렉티브 디자인/static/js/char.js
+++ 02 인터렉티브 디자인/static/js/char.js
@@ -1,17 +1,3 @@
-/*/ Create a circle shaped path with its center at the center
-// of the view and a radius of 30:
-var path = new Path.Circle({
-	center: view.center,
-	radius: 30,
-	strokeColor: 'black'
-});
-
-function onResize(event) {
-	// Whenever the window is resized, recenter the path:
-	path.position = view.center;
-}
-*/
-// Only executed our code once the DOM is ready.
 window.onload = function() {
     // Get a reference to the canvas object
     var canvas = document.getElementById('myCanvas');
